ef46bad318061643db49dc4e481385470cdc4fc4
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-26 12:58:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,21 @@ if (!databaseUri) {
 
 var api = new ParseServer({
   databaseURI: databaseUri || process.env.MONGODB_URI,
-  cloud: process.env.CLOUD_CODE_MAIN,
-  //|| __dirname + '/cloud/main.js',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || '', // Don't forget to change to https if needed
-  push: {
+  liveQuery: {
+  classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+    push: {
         ios: 
         {
             pfx: '/app/ProdPush.p12', // the path and filename to the .p12 file you exported earlier. 
             topic: 'com.riadco.kaman', // The bundle identifier associated with your app
             production: true // Specifies which environment to connect to: Production (if true) or Sandbox
         }
-    },
-  liveQuery: {
-  classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
-
-
+    }
 });
  
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
@@ -53,18 +50,18 @@ app.get('/', function(req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+//app.get('/test', function(req, res) {
+ // res.sendFile(path.join(__dirname, '/public/test.html'));
+//});
 
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
-    console.log('Andrew'+process.env.MASTER_KEY);
-  console.log('TEST CLOUD'+(process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js'));
-  console.log('Andrew 2 '+__dirname);
-});
+//var port = process.env.PORT || 1337;
+//var httpServer = require('http').createServer(app);
+//httpServer.listen(port, function() {
+  //  console.log('parse-server-example running on port ' + port + '.');
+//    console.log('Andrew'+process.env.MASTER_KEY);
+ // console.log('TEST CLOUD'+(process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js'));
+//  console.log('Andrew 2 '+__dirname);
+//});
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
