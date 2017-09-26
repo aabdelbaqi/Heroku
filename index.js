9b79954a49f85debb9c417f65287712ff7876a69
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-26 13:10:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || process.env.MONGODB_URI,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '',
+  appId: process.env.APP_ID || '1vbNptEzFhOptvNm0cs0Gud8kVCFMg4LjyczEcXh',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || '', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://kaman-os-2.herokuapp.com/parse', // Don't forget to change to https if needed
   liveQuery: {
   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-    push: {
+  push: {
         ios: 
         {
             pfx: '/app/ProdPush.p12', // the path and filename to the .p12 file you exported earlier. 
