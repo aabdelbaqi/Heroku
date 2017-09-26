a6364420be6bf793e3c34325104b0f1236f50f4a
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-26 14:35:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || '', // Don't forget to change to https if needed
   liveQuery: {
   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
   push: {
         ios: 
         {
