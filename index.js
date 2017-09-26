dce77b1517a4e19d0541778186bad8a047ef9fd2
aabdelbaqi <ahmed.abdelbaqi@gmail.com>
2017-09-26 12:44:02 +0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,15 @@ var api = new ParseServer({
   //|| __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || '',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || '',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || '', // Don't forget to change to https if needed
+  push: {
+        ios: 
+        {
+            pfx: '/app/ProdPush.p12', // the path and filename to the .p12 file you exported earlier. 
+            topic: 'com.riadco.kaman', // The bundle identifier associated with your app
+            production: true // Specifies which environment to connect to: Production (if true) or Sandbox
+        }
+    },
   liveQuery: {
   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
